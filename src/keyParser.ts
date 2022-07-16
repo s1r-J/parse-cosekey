e60f5e8cb90c16ccafc99135bf86f54b607cd9e3
s1r-J <s1r.aria.azureglo@gmail.com>
2022-07-17 01:13:04 +0900

    Fix JWK x property must be base64url #11

--- src/keyParser.ts
+++ src/keyParser.ts
@@ -31,7 +31,34 @@ import {
 type coseMap = Map<number, any>;
 
 type jwk = {
-  [key: string]: any;
+  kty?: string;
+  use?: string;
+  key_ops?: string[];
+  alg?: string;
+  kid?: string;
+  x5u?: string;
+  x5c?: string[];
+  x5t?: string;
+  'x5t#S256'?: string;
+  crv?: string;
+  x?: string;
+  y?: string;
+  d?: string;
+  n?: string;
+  e?: string;
+  p?: string;
+  q?: string;
+  dp?: string;
+  dq?: string;
+  qi?: string;
+  oth?: {
+    r?: string;
+    d?: string;
+    t?: string;
+  }[];
+  k?: string;
+  ext?: boolean;
+  [key: string]: unknown;
 };
 
 type pem = string;
